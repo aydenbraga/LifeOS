99fb1c14340d73e9b5fa888652ca605ebc3999d6
aydenbraga <ayden.braganza@gmail.com>
2026-06-02 02:05:09 +1000

    fix: remove unsupported eslint config from next.config.mjs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to complete even if
-    // your project has type errors.
-    // cheers to a successful deploy!
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Warning: This allows production builds to complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
   },
 };
 
